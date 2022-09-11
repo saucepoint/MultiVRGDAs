505d49c73bd3dd3645c5c511687cd37595ae8c0f
saucepoint <saucepoint@protonmail.com>
2022-09-11 13:12:44 -0400

    examples for multi VRGDA usage

---
+++ src/examples/MultiNFT.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+import {toDaysWadUnsafe, toWadUnsafe, unsafeWadDiv, wadLn, unsafeDiv} from "solmate/utils/SignedWadMath.sol";
+
+import {LinearVRGDALib, LinearVRGDAx} from "../lib/LinearVRGDALib.sol";
+import {LogisticVRGDALib, LogisticVRGDAx} from "../lib/LogisticVRGDALib.sol";
+
+/// @title Multi VRGDA NFT
+/// @author transmissions11 <t11s@paradigm.xyz>
+/// @author FrankieIsLost <frankie@paradigm.xyz>
+/// @author saucepoint
+/// @notice Example NFT sold using BOTH Linear VRGDA and Logistic VRGDA.
+/// @dev This is an example. Do not use in production.
+contract MultiNFT is ERC721 {
+    using LinearVRGDALib for LinearVRGDAx;
+    using LogisticVRGDALib for LogisticVRGDAx;
+    /*//////////////////////////////////////////////////////////////
+                              SALES STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 public totalSold; // The total number of tokens sold so far.
+
+    uint256 public immutable startTime = block.timestamp; // When Linear VRGDA sales begin.
+
+    uint256 public immutable publicStartTime = block.timestamp + 30 days; // When Logistic VRGDA sales begin.
+
+    // -- VRGDA Objects --
+    LinearVRGDAx internal presaleVRGDA;
+    LogisticVRGDAx internal publicVRGDA;
+
+    // -- Linear VRGDA Params --
+    int256 public perTimeUnit;  // The number of tokens to target selling in 1 full unit of time, scaled by 1e18.
+
+    // -- Logistic VRGDA Params --
+    uint256 public constant MAX_MINTABLE = 100; // Max supply. for logistic VRGDA
+
+    /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor()
+        ERC721(
+            "Example Multi VRGDA NFT", // Name.
+            "mVRGDA" // Symbol.
+        )
+    {
+        // -------------------------
+        // create a VRGDA to be used in presale
+        // -------------------------
+        presaleVRGDA = LinearVRGDALib.createLinearVRGDA(
+            69.42e18, // Target price.
+            0.31e18, // Price decay percent.
+            2e18  // 2 units sold per unit of time
+        );
+
+        // -----------------------------
+        // create a VRGDA to used in public sale
+        // note: we can reuse presaleVRGDA and overwrite the .getTargetSaleTime since
+        // they are used during different times. However for the sake of example, let's define two independent VRGDAs
+        // -----------------------------
+        publicVRGDA = LogisticVRGDALib.createLogisticVRGDA(
+            69.42e18, // Target price.
+            0.31e18, // Price decay percent.
+            // Maximum # mintable/sellable.
+            toWadUnsafe(MAX_MINTABLE),
+            0.1e18 // Time scale.
+        );
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              MINTING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function mint() external payable returns (uint256 mintedId) {
+        unchecked {
+            // mintedId = totalSold++;
+            // conditionally set price based on time + VRGDA
+            // i.e. if time < publicStartTime use presaleVRGDA else use publicVRGDA
+            uint256 price;
+
+            if (block.timestamp < publicStartTime) {
+                // Note: By using toDaysWadUnsafe(block.timestamp - startTime) we are establishing that 1 "unit of time" is 1 day.
+                price = presaleVRGDA.getVRGDAPrice(toDaysWadUnsafe(block.timestamp - startTime), mintedId = totalSold++);
+            } else {
+                price = publicVRGDA.getVRGDAPrice(toDaysWadUnsafe(block.timestamp - publicStartTime), mintedId = totalSold++);
+            }
+
+            require(msg.value >= price, "UNDERPAID"); // Don't allow underpaying.
+
+            _mint(msg.sender, mintedId); // Mint the NFT using mintedId.
+
+            // Note: We do this at the end to avoid creating a reentrancy vector.
+            // Refund the user any ETH they spent over the current price of the NFT.
+            // Unchecked is safe here because we validate msg.value >= price above.
+            SafeTransferLib.safeTransferETH(msg.sender, msg.value - price);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                URI LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return "https://example.com";
+    }
+}
---
+++ test/Contract.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "forge-std/Vm.sol";
+
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+
+import {Contract} from "../src/examples/Contract.sol";
+
+contract ContractTest is DSTestPlus {
+    Contract c;
+
+    function setUp() public {
+        c = new Contract();
+    }
+
+    function rng(uint256 a, uint256 b) public pure returns (uint256) {
+        return (uint256(keccak256(abi.encodePacked(a, b))) % 10) + 1;
+    }
+
+    // A simple test to make sure the contract is not reverting
+    // VRGDA math is verified by other tests
+    // This test also helps sample gas usage for `forge test --gas-report`
+    function testGasConsumption() public {
+        uint256 price = c.getPriceLinear();
+        c.buyLinear{value: price}(1);
+
+        price = c.getPriceLogistic();
+        c.buyLogistic{value: price}(1);
+
+        uint256 amount;
+        uint256 total;
+        for (uint256 i = 0; i < 20; i++) {
+            price = c.getPriceLinear();
+            amount = rng(i, price);
+            total = price * amount;
+            
+            c.buyLinear{value: total}(amount);
+            hevm.warp(block.timestamp + rng(i, block.timestamp));
+        }
+
+        for (uint256 i = 0; i < 20; i++) {
+            price = c.getPriceLogistic();
+            amount = rng(i, price);
+            total = price * amount;
+
+            c.buyLogistic{value: total}(amount);
+            hevm.warp(block.timestamp + rng(i, block.timestamp));
+        }
+    }
+}
